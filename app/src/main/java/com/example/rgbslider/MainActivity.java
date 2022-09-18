7ded43656c902b09399cb398adf32ab49d05f2dd
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-18 11:56:29 +0200

    Mainactivity init

--- app/src/main/java/com/example/rgbslider/MainActivity.java
+++ app/src/main/java/com/example/rgbslider/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.rgbslider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout layout;
+    private TextView redText;
+    private TextView greenText;
+    private TextView blueText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init(){
+        layout = findViewById(R.id.layout);
+        redText = findViewById(R.id.redText);
+        greenText = findViewById(R.id.greenText);
+        blueText = findViewById(R.id.blueText);
     }
 }
